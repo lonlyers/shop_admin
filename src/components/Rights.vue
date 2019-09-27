e3e4538ceaa9fc150ec9cccea20bc31f350682c5
lonyer <zw1503637389@163.com>
2019-09-27 16:32:33 +0800

    权限列表已完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,15 +1,64 @@
 <template>
   <div class="rights">
-    这是right组件
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-table
+      ref="singleTable"
+      :data="tableData"
+      style="width: 100%"
+    >
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column property="authName" label="权限名称" width="400"></el-table-column>
+      <el-table-column property="path" label="路径" width="400"></el-table-column>
+      <el-table-column  label="层级">
+        <template v-slot:default="{ row: { level }}">
+          <span v-if="level === '0'">一级</span>
+          <span v-else-if="level === '1'">二级</span>
+          <span v-else>三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  async created () {
+    const { data } = await this.$axios.get('rights/list')
+    console.log(data)
 
+    this.tableData = data
+  }
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+.el-breadcrumb {
+  line-height: 40px;
+  border-bottom: 1px solid #ccc;
+}
 </style>
